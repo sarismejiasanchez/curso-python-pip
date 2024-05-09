977922f60f7b5ef7828763e9b3d6798710d32625
sarismejiasanchez <sarismejiasanchez@gmail.com>
2024-05-08 22:56:21 -0500

    Python for Backend: web server with FastAPI

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,28 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+# Creamos una instancia de la aplicacion
+app = FastAPI()
+
+# Creamos un decorador, con la ruta desde la cual se accederá desde la web
+@app.get('/')
+# Creamos un recurso
+def get_list():
+    return [1, 2, 3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_contact():
+    return """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
 
 def run():
     store.get_categories()
